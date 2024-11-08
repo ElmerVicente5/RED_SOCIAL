00f5e53c466d6bd1c6e9ff5f88faa06fcbc661fd
Elmer <vicente.pelico305@gmail.com>
2024-11-08 16:13:42 -0600

    fix: change ip adrres for the page

--- src/Social-Media-App-Frontend/src/config/config.js
+++ src/Social-Media-App-Frontend/src/config/config.js
@@ -1,4 +1,4 @@
 // config.js
-export const API_URL = "http://localhost:8000"; // Cambiar según la IP o el dominio
-//export const API_URL = "http://20.3.250.158:8000"; // Cambiar según la IP o el dominio
+//export const API_URL = "http://localhost:8000"; // Cambiar según la IP o el dominio
+export const API_URL = "http://20.3.250.158:8000"; // Cambiar según la IP o el dominio
 
